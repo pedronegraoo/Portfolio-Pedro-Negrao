0d6635c6a1003b8d58204eed2cc5fabe6669c16d
Pedro Negrao <macedopedro117@yahoo.com.br>
2023-11-27 22:42:17 -0300

    Criação do arquivo index.js

---
+++ src/js/index.js
@@ -0,0 +1,86 @@
+const empresarial = document.getElementById("empresarial");
+const blog = document.getElementById("blog");
+const nutriCalc = document.getElementById("nutri");
+const previsao = document.getElementById("previsao");
+const menuEmpresarial = document.querySelector(".redirecionar-empresarial");
+const menuNutri = document.querySelector(".redirecionar-nutri");
+const menuBlog = document.querySelector(".redirecionar-blog");
+const menuPrevisao = document.querySelector(".redirecionar-previsao");
+const overlay = document.querySelector(".overlay-projetos");
+
+// ABRIR MENU DOS PROJETOS
+
+empresarial.addEventListener("click", () => {
+  overlay.style.display = "block";
+  menuEmpresarial.classList.add("active");
+});
+
+blog.addEventListener("click", () => {
+  overlay.style.display = "block";
+  menuBlog.classList.add("active");
+});
+
+nutriCalc.addEventListener("click", () => {
+  overlay.style.display = "block";
+  menuNutri.classList.add("active");
+});
+
+previsao.addEventListener("click", () => {
+  overlay.style.display = "block";
+  menuPrevisao.classList.add("active");
+});
+
+// FECHAR MENU PROJETOS
+
+menuEmpresarial.addEventListener("click", () => {
+  menuEmpresarial.classList.remove("active");
+  overlay.style.display = "none";
+});
+
+menuNutri.addEventListener("click", () => {
+  menuNutri.classList.remove("active");
+  overlay.style.display = "none";
+});
+
+menuBlog.addEventListener("click", () => {
+  menuBlog.classList.remove("active");
+  overlay.style.display = "none";
+});
+
+menuPrevisao.addEventListener("click", () => {
+  menuPrevisao.classList.remove("active");
+  overlay.style.display = "none";
+});
+
+overlay.addEventListener("click", () => {
+  menuEmpresarial.classList.remove("active");
+  menuNutri.classList.remove("active");
+  menuBlog.classList.remove("active");
+  menuPrevisao.classList.remove("active");
+  overlay.style.display = "none";
+});
+
+// ABRIR MENU LATERAL MOBILE
+
+const btnAbrirMenu = document.querySelector(".btn-abrir-menu-mobile");
+const MenuMobile = document.querySelector(".menu-mobile");
+const overlayMenuMobile = document.querySelector(".overlay-menu-mobile");
+
+// Abrir Menu
+
+btnAbrirMenu.addEventListener("click", () => {
+  overlayMenuMobile.style.display = "block";
+  MenuMobile.classList.add("abrir-menu");
+});
+
+// Fechar menu
+
+MenuMobile.addEventListener("click", () => {
+  overlayMenuMobile.style.display = "none";
+  MenuMobile.classList.remove("abrir-menu");
+});
+
+overlayMenuMobile.addEventListener("click", () => {
+  MenuMobile.classList.remove("abrir-menu");
+  overlayMenuMobile.style.display = "none";
+});
